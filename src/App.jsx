3f00cbae574bf8478ad94f769619292ed78bcd1c
gelarahadian <gelar.rahadian.fajar@gmail.com>
2025-12-16 08:40:08 +0700

    Add: component EvenBusProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
 import { QueryProvider } from "./components/providers/QueryProvider";
 import ToasterProvider from "./components/providers/ToasterProvider";
+import EventBusProvider from "./components/providers/EventBusProvider";
 
 function App() {
   return (
     <div>
       <QueryProvider>
         <ToasterProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<SplashScreen />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/forgot-password" element={<ForgotPassword />} />
-              <Route path="/reset-password" element={<ResetPassword />} />
-            </Routes>
-          </Router>
+          <EventBusProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<SplashScreen />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/reset-password" element={<ResetPassword />} />
+              </Routes>
+            </Router>
+          </EventBusProvider>
         </ToasterProvider>
       </QueryProvider>
     </div>
